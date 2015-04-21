6598d8b4253d7aec32f78fbff67a7557f194cef5
[daniel234] <daniele.rb6@gmail.com>
2015-04-21 12:21:39 +0200

    Add pattern count algorithm

---
+++ pattern_count.py
@@ -0,0 +1,30 @@
+def pattern_count(text, pattern):
+    ''' (str, str) -> (int)
+
+    Return how many times a given pattern appears in text. The program
+    accounts for overlapping occurrences.
+    Assume len(text) > 0.
+
+    >>> pattern_count('GCGCG', '')
+    0
+    >>> pattern_count('GCGCG', 'AT')
+    0
+    >>> pattern_count('G', 'AT')
+    0
+    >>> pattern_count('GCGCG', 'GCG')
+    2
+        
+    '''
+
+    count = 0
+    i = 0
+
+    if pattern == '' or (len(text) < len(pattern)):
+        return 0
+    else:
+        while(i < (len(text) - len(pattern) + 1)):
+            if pattern == text[i:i + 3]:
+                count += 1
+            i += 1
+    
+    return count
